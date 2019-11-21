ee72461ac57f482001b9ce6bb11b6d930a125b3f
build-failure <michael@lewkowski.de>
2019-11-21 14:56:08 +0100

    Prevent word splitting

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -6,6 +6,6 @@ info "Set up SSH agent with the supplied key."
 mkdir -p /root/.ssh
 cp /tmp/.ssh/* /root/.ssh/
 chmod 600 /root/.ssh/*
-eval $(ssh-agent) &>/dev/null \
+eval "$(ssh-agent)" &>/dev/null \
     && ssh-add &>/dev/null \
     && bash -c "$*"
