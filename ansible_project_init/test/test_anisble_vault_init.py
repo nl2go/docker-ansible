b4d8dabb066b135d322378be9b86c8011e79352b
build-failure <michael@lewkowski.de>
2019-11-25 14:18:15 +0100

    Add directory scan test

--- ansible_project_init/test/test_anisble_vault_init.py
+++ ansible_project_init/test/test_anisble_vault_init.py
@@ -1,6 +1,7 @@
 import sys
 import unittest
 import tempfile
+import os
 from unittest import mock
 
 sys.modules['getpass'] = mock.MagicMock()
@@ -15,12 +16,12 @@ class AnsibleVaultInitTest(unittest.TestCase):
         self.assertEqual('foo@bar', ansible_vault_init.get_vault_id('foo', 'bar'))
 
     def test_encrypt_vault_password(self):
-        vault_password = 'foo'
+        expected_vault_password = 'foo'
         encryption_password = 'bar'
         vault_password_file = tempfile.NamedTemporaryFile('r')
         encrypted_vault_password_file = tempfile.NamedTemporaryFile('r')
         ansible_vault_init.encrypt_vault_password_file(
-          vault_password,
+          expected_vault_password,
           encryption_password,
           encrypted_vault_password_file.name
         )
@@ -32,7 +33,7 @@ class AnsibleVaultInitTest(unittest.TestCase):
         )
 
         actual_vault_password = vault_password_file.read()
-        self.assertEqual(vault_password, actual_vault_password.rstrip())
+        self.assertEqual(expected_vault_password, actual_vault_password.rstrip())
         encrypted_vault_password_file.close()
         vault_password_file.close()
 
@@ -43,6 +44,19 @@ class AnsibleVaultInitTest(unittest.TestCase):
         expected_encryption_password = 'foo'
         getpass.getpass.return_value = expected_encryption_password
 
-        encryption_password = ansible_vault_init.prompt_encryption_password()
+        actual_encryption_password = ansible_vault_init.prompt_encryption_password()
 
-        self.assertEqual(expected_encryption_password, encryption_password)
+        self.assertEqual(expected_encryption_password, actual_encryption_password)
+
+    def test_get_encrypted_vault_password_files(self):
+        base_dir = tempfile.TemporaryDirectory('r')
+        inventory_dir = base_dir.name + '/inventories/foo'
+        os.makedirs(inventory_dir, exist_ok=True)
+        open(inventory_dir + '/.vault-password', 'a').close()
+
+        actual_encrypted_vault_password_files = ansible_vault_init.get_encrypted_vault_password_files(base_dir.name)
+
+        self.assertEqual([base_dir.name + '/inventories/foo/.vault-password'], actual_encrypted_vault_password_files)
+        base_dir.cleanup()
+
+
