f925bbc54a4fa0480c7e2c02228ea393c9308b20
build-failure <michael@lewkowski.de>
2019-11-25 15:24:00 +0100

    Fix test and format

--- ansible_project_init/test/test_ansible_galaxy_init.py
+++ ansible_project_init/test/test_ansible_galaxy_init.py
@@ -17,14 +17,19 @@ class AnsibleGalaxyInitTest(unittest.TestCase):
         requirements_file = role_dir + '/requirements.yml'
         open(requirements_file, 'a').close()
         mock_os_getcwd.return_value = base_dir.name
-        mock_subprocess_call.side_effect = (lambda command: self.assertEqual(command, 'ansible-galaxy install -r ' + requirements_file))
+        mock_subprocess_call.side_effect = (
+            lambda command: self.assertEqual(
+                command,
+                'ansible-galaxy install -r ' + requirements_file
+            )
+        )
 
         ansible_galaxy_init.init()
 
         base_dir.cleanup()
 
     @mock.patch('os.getcwd')
-    def test_init(self, mock_os_getcwd):
+    def test_init_skip(self, mock_os_getcwd):
         base_dir = '/tmp'
         mock_os_getcwd.return_value = base_dir
 
