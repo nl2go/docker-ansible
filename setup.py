a21ea9ee5950ae600ccc5efa4c551a41ccbf1488
build-failure <michael@lewkowski.de>
2019-11-27 08:39:17 +0100

    Fix linter issues

--- setup.py
+++ setup.py
@@ -6,10 +6,13 @@ setup(
     entry_points={
         'console_scripts': [
             'ansible-project-init = ansible_project_init.__main__:init',
-            'ansible-vault-init = ansible_project_init.ansible_vault_init:init',
-            'ansible-galaxy-init = ansible_project_init.ansible_galaxy_init:init',
+            'ansible-vault-init = '
+            'ansible_project_init.ansible_vault_init:init',
+            'ansible-galaxy-init = '
+            'ansible_project_init.ansible_galaxy_init:init',
             'ssh-agent-init = ansible_project_init.ssh_agent_init:init',
-            'ansible-encrypt-vault-password = ansible_project_init.ansible_vault_crypt:encrypt_vault_password'
+            'ansible-encrypt-vault-password = '
+            'ansible_project_init.ansible_vault_crypt:encrypt_vault_password'
         ],
     }
 )
